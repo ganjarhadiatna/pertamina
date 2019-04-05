f17237aef3fe29598ab2d2ab4140890d8f4eb4df
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2019-04-05 11:13:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+	
+	<router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  /* @import url('./assets/icons/fontawesome/css/all.min.css'); */
+  @import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+  @import url('./assets/scss/main.css');
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="app-login">
+        <div class="a-l-top">
+            <div class="a-l-logo">
+                <img src="@/assets/img/logo.png">
+            </div>
+        </div>
+        <div class="a-l-mid">
+            <div class="a-l-form">
+                <div class="a-l-input bdr-bottom">
+                    <div class="icn">
+                        <i class="fa fa-lg fa-user"></i>
+                    </div>
+                    <input 
+                        type="text" 
+                        class="txt txt-primary" 
+                        placeholder="Username">
+                </div>
+                <div class="a-l-input">
+                    <div class="icn">
+                        <i class="fa fa-lg fa-key"></i>
+                    </div>
+                    <input 
+                        type="password" 
+                        class="txt txt-primary" 
+                        placeholder="Password">
+                </div>
+            </div>
+            <div class="a-l-button">
+                <button 
+                    type="submit" 
+                    class="btn btn-sekunder-color btn-radius btn-width-all">
+                    LOGIN
+                </button>
+            </div>
+        </div>
+        <div class="a-l-bot">
+            <div class="txt-site txt-center txt-11 txt-white">
+                Powered by Bitozen
+            </div>
+        </div>
+    </div>
+</template>
